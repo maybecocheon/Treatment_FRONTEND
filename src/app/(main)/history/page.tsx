983d3ffa0f671a4f070268398dfb1085ea2e08f8
maybecocheon <guswl293@gmail.com>
2026-03-05 12:12:28 +0900

    Add overflow-x-hidden to HistoryPage container

--- src/app/(main)/history/page.tsx
+++ src/app/(main)/history/page.tsx
@@ -2,7 +2,7 @@ import History from "./History";
 
 export default function HistoryPage() {
   return (
-    <div className="flex flex-col h-full gap-3 md:p-4">
+    <div className="flex flex-col h-full gap-3 md:p-4 overflow-x-hidden">
       <History />
     </div>
   );
